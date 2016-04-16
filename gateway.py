8133e951d7f88ebe4410246f6a3f9783974d4291
Chad S. Lauritsen <chad.s.lauritsen@sherwin.com>
2016-04-16 00:22:10 -0400

    translates to human-readable string, at least output from gateway is working ok

--- gateway.py
+++ gateway.py
@@ -1,90 +1,157 @@
 #!/usr/bin/python 
 
+import mtypes
 import serial
 import time
 import io
 import Queue as queue
 import paho.mqtt.client as mqtt
+import signal
+import getopt
+import sys
 
-x=0
+isInfo=True
+
+def info(line):
+    global isInfo
+    if isInfo:
+        print line
+
+def debug(line):
+    global isDebug
+    if isDebug:
+        print line
+
+def sighndlr(signal, frame):
+    global go
+    print 'received signal %d' % (signal)
+    go=False
+
+
+def on_message(client, userdata, msg):
+    q.put(msg)
+
+def on_publish(client, userdata, mid):
+    pass
+
+def m2g(msg):
+    tp = msg.topic.split('/')
+    node_id=tp[2]
+    child_id=tp[3]
+    msg_type_s=tp[4]
+    ack_s=tp[5]
+    sub_type_s=tp[6]
+    msg_type = mtypes.message_types[msg_type_s]
+    if msg_type == mtypes.M_INTERNAL:
+        sub_type = mtypes.internal_sub_types[sub_type_s]
+    elif msg_type == mtypes.M_SET:
+        sub_type = mtypes.set_sub_types[sub_type_s]
+    elif msg_type == mtypes.M_PRESENTATION:
+        sub_type = mtypes.presentation_sub_types[sub_type_s]
+    else:
+        print 'WARNING unknown message sub type %s' % sub_type_s
+
+    print 'SWRITE: %d;%d;%d;%d;%d;%s' % (int(node_id), int(child_id), msg_type, int(ack_s), sub_type, msg.payload)
+
+def g2m(line):
+    debug(line)
+    try:
+        node_id, child_sensor_id, message_type, ack, sub_type, payload = line.split(';', 5)
+        message_type = int(message_type)
+        sub_type = int(sub_type)
+        topic='pmmg/out'
+        topic = topic + '/' + node_id
+        topic = topic + '/' + child_sensor_id
+        topic = topic + '/' + mtypes.message_types[message_type]
+        if message_type == mtypes.M_PRESENTATION:
+            topic = topic + '/' + mtypes.presentation_sub_types[sub_type]
+        elif message_type == mtypes.M_SET:
+            topic = topic + '/' + mtypes.set_sub_types[sub_type]
+        qos=2
+        retain=False
+        mqttc.publish(topic, payload, qos, retain)
+    except ValueError:
+        pass
+
 go=True
 
 ser=None
 sio=None
 sConnected=False
+isDebug=False
+host='localhost'
+port=1883
+serialport=''
+baud=115200
 
-def on_message(client, userdata, msg):
-    q.put(msg)
-    print 'CB: ', str(msg.payload)
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "b:dh:p:s:", ["baud=", "debug", "host=", "port=", "serial="])
+except getopt.GetoptError as err:
+    print str(err)
+    sys.exit(2)
 
-def on_publish(client, userdata, mid):
-    print 'OP: ', mid
+for o, a in opts:
+    if o == "-b":
+        baud = int(a)
+    elif o == "-d":
+        isDebug = True
+    elif o == "-h":
+        host = a
+    elif o == "-p":
+        port = int(a)
+    elif o == "-s":
+        serialport = a
+    else:
+        assert False, "unhandled option"
 
-def m2g(to, msg):
-    print 'SWRITE:', to, msg
+signal.signal(signal.SIGTERM, sighndlr)
 
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
-mqttc.on_publish = on_publish
+#mqttc.on_publish = on_publish
 
 q = queue.Queue()
-while go:
-    try:
-        ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-        sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-        print 'Connected to Serial'
-        sConnected=True
-        mqttc.connect('localhost', 1883)
-        mqttc.subscribe('pmmg/in/#', qos=2)
-        x=0
-        while sConnected and x < 10:
-            try:
-                mqttc.loop_misc()
-                print 'LM:'
-                line = sio.readline().rstrip()
-                if line != '':
-                    #node_id, child_sensor_id, message_type, ack, sub_type, payload = line.split(';')
-                    fields = line.split(';')
-                    if len(fields) == 6:
-                        topic='pmmg/out'
-                        topic = topic + '/' + fields[0]
-                        topic = topic + '/' + fields[1]
-                        topic = topic + '/' + fields[2]
-                        topic = topic + '/' + fields[3]
-                        topic = topic + '/' + fields[4]
-                        mqttc.publish(topic, fields[5], 2, True)
-                    print 'SREAD: ', line
-                x = x + 1
-                mqttc.loop_write()
-                print 'LW:'
-                while True:
-                    mqttc.loop_read()
-                    print 'LR:'
-                    try:
-                       m = q.get(False)
-                       m2g('fred', m.payload)
-                       q.task_done()
-                    except queue.Empty:
-                       break
-
-            except IOError as ioe:
-                print 'IOError', ioe
-            except serial.SerialException:
-                print 'Device disconnected'
-                sConnected=False
-    except KeyboardInterrupt:
-        print 'Quitting'
-        go=False
-    except OSError as ose:
-        time.sleep(1)
-        print 'No can connect', ose
-    except serial.SerialException as e:
-        print e
-    finally:
-        if ser and not ser.closed:
-            ser.close()
-            print 'Closed ser'
+try:
+    #ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser = serial.Serial(serialport, baud, timeout=1)
+    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+    info('Connected to Serial')
+    sConnected=True
+    mqttc.connect(host, port)
+    mqttc.subscribe('pmmg/in/#', qos=2)
+    while go and sConnected:
+        try:
+            mqttc.loop_misc()
+            line = sio.readline().rstrip()
+            if line != '':
+                g2m(line)
+            mqttc.loop_write()
+            while True:
+                mqttc.loop_read()
+                try:
+                   m = q.get(False)
+                   m2g(m)
+                   q.task_done()
+                except queue.Empty:
+                   break
+        except IOError as ioe:
+            print 'IOError', ioe
+        except serial.SerialException:
+            print 'Device disconnected'
+            sConnected=False
+except KeyboardInterrupt:
+    print 'Quitting'
+    go=False
+except OSError as ose:
+    time.sleep(1)
+    print 'No can connect', ose
+except serial.SerialException as e:
+    print e
+finally:
+    if ser and not ser.closed:
+        ser.close()
+        print 'Closed serial port'
         mqttc.disconnect()
-        print 'disc mqtt'
+        print 'disconnected from mqtt broker'
 
 print 'bye'
